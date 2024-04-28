b832731d590f323c962b2db132ba82ed4424f087
Axel Flores Teja <axelfloresteja@gmail.com>
2024-04-28 05:02:17 -0600

    added 'add' and complete 'commands'

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 default:
-	clang -O3 main.c -o main.o
+	clang -O3 main.c -o bin/main
--- main.c
+++ main.c
@@ -3,14 +3,63 @@
 #include <string.h>
 
 int main(int argc, char** argv) {
-  FILE *fptr;
-  char line[100];
+  const char* TODO_PATH = getenv("TODO_PATH");
 
-  fptr = fopen("sad.txt", "a+");
-
-  if(fptr == NULL) {
-    fprintf(stderr, "%s", "Could not open file\n");
+  if(argc != 3) {
+    fprintf(stderr, "%s", "Not enough arguments");
+    exit(-1);
   }
 
-  fclose(fptr); 
+  FILE *fptr;
+  FILE *fptrw;
+
+  int count = 0;
+  char line[255];
+  char* command = argv[1];
+
+  char target[100];
+  
+  fptrw = fopen("tmp.txt", "w+");
+  fptr = fopen(TODO_PATH, "a+");
+
+  if(fptr == NULL || fptrw ==NULL) {
+    fprintf(stderr, "%s", "Could not open file\n");
+    exit(-1);
+  }
+  
+  if(!strcmp("add", command)) {
+    char* text = argv[2];
+
+    fprintf(fptr, "[ ] %s\n", text);
+
+  } else if(!strcmp("complete", command)) {
+    int numLine = atoi(argv[2]);
+
+    while(fgets(line, 255, fptr)) {
+      if(count == numLine) { 
+
+        snprintf(target, sizeof target, "%.3s", line);
+        if(!strcmp(target, "[ ]")) {
+
+          char* title = line + 4;
+
+          fprintf(fptrw, "[*] %s", title);
+        }
+      } else {
+
+        fputs(line, fptrw);
+      }
+
+      count++;
+    }
+  }
+
+  fclose(fptr);
+  fclose(fptrw);
+
+  remove(TODO_PATH);
+
+  rename("tmp.txt", TODO_PATH);
+
+  return 0;
 }
