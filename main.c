45e36bf4e77e8c6f4001eb2eec429a45f36b65b7
Axel Flores Teja <axelfloresteja@gmail.com>
2024-04-29 15:49:26 -0600

    added list command

--- main.c
+++ main.c
@@ -2,12 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CLEAR_COLOR "\x1B[0m"
+#define GREEN "\x1B[32m"
+#define RED "\x1B[31m"
+
 int main(int argc, char** argv) {
   const char* TODO_PATH = getenv("TODO_PATH");
 
-  if(argc != 3) {
-    fprintf(stderr, "%s", "Not enough arguments");
+  if(argc != 3 && !strcmp("add", argv[1])) {
+    fprintf(stderr, "%s", "Problem parsing arguments");
     exit(-1);
+
+  } else if(argc != 3 && !strcmp("complete", argv[1])) {
+    fprintf(stderr, "%s", "Problem parsing arguments");
+    exit(-1);
+
+  } else if(argc != 2 && !(strcmp("list", argv[1]))) {
+    fprintf(stderr, "%s", "Problem parsing arguments");
+    exit(-1);
+
   }
 
   FILE *fptr;
@@ -18,8 +31,7 @@ int main(int argc, char** argv) {
   char* command = argv[1];
 
   char target[100];
-  
-  
+    
   fptr = fopen(TODO_PATH, "a+");
   fptrw = fopen("tmp.txt", "w+");
 
@@ -38,8 +50,12 @@ int main(int argc, char** argv) {
     int numLine = atoi(argv[2]);
 
     while(fgets(line, 255, fptr)) {
-      if(count == numLine) { 
-        snprintf(target, sizeof target, "%.3s", line);
+      if(numLine < 1) {
+        fprintf(stderr, "%s", "Index bellow 1");
+        exit(-1);
+      }
+      if(count == numLine - 1) { 
+        snprintf(target, sizeof(target), "%.3s", line);
         if(!strcmp(target, "[ ]")) {
           char* title = line + 4;
 
@@ -56,6 +72,20 @@ int main(int argc, char** argv) {
       count++;
     }
     remove(TODO_PATH);
+    rename("tmp.txt", TODO_PATH);
+
+  } else if(!strcmp("list", command)) {
+    while(fgets(line, 255, fptr)) {
+      char* title = line + 4;
+
+      snprintf(target, sizeof(target), "%.3s", line);
+
+      if(!strcmp(target, "[*]")) {
+        printf("%s %s%s",GREEN , title, CLEAR_COLOR);
+      } else {
+        printf("%s %s%s",RED , title, CLEAR_COLOR);
+      }
+    }
     rename("tmp.txt", TODO_PATH);
   }
 
